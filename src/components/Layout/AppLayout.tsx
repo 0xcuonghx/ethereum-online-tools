75f8b706aaf7a626dbd419f55845fa41b64a762d
0xcuonghx <cuonghx@gu-tech.com>
2024-05-08 22:14:30 +0700

    Update styles

--- src/components/Layout/AppLayout.tsx
+++ src/components/Layout/AppLayout.tsx
@@ -1,11 +1,21 @@
-import { Layout } from "antd";
+import { Layout, Typography } from "antd";
 import React, { PropsWithChildren } from "react";
+import { useNavigate } from "react-router-dom";
 
 const { Header, Content, Footer } = Layout;
 const AppLayout: React.FC<PropsWithChildren> = ({ children }) => {
+  const navigate = useNavigate();
   return (
     <Layout>
-      <Header></Header>
+      <Header style={{ alignItems: "center", display: "flex" }}>
+        <Typography.Title
+          level={3}
+          style={{ color: "white", margin: 0, cursor: "pointer" }}
+          onClick={() => navigate("/")}
+        >
+          Ethereum Online Tools
+        </Typography.Title>
+      </Header>
       <Content>
         <div
           style={{
