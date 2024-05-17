f216484ccf42065e2657693f4471d34fdbfdcdd5
0xcuonghx <cuonghx@gu-tech.com>
2024-05-17 16:14:54 +0700

    Change home layout

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -5,13 +5,16 @@ import { useNavigate } from "react-router-dom";
 const HomePage = () => {
   const navigate = useNavigate();
 
-  const tools = [
+  const accountTools = [
     {
       title: "Private Key to Public Key",
     },
     {
       title: "Seed phrase to Private Key",
     },
+  ];
+
+  const utilsTools = [
     {
       title: "Gwei Calculator",
     },
@@ -28,8 +31,23 @@ const HomePage = () => {
   return (
     <div>
       <Typography.Title>Ethereum Online Tools</Typography.Title>
+      <Typography.Title level={2}>Account Tools</Typography.Title>
       <Row gutter={16}>
-        {tools.map(({ title }) => (
+        {accountTools.map(({ title }) => (
+          <Col className="gutter-row" span={6}>
+            <Card
+              bordered={false}
+              hoverable
+              onClick={() => handleChooseTool(title)}
+            >
+              {title}
+            </Card>
+          </Col>
+        ))}
+      </Row>
+      <Typography.Title level={2}>Utils Tools</Typography.Title>
+      <Row gutter={16}>
+        {utilsTools.map(({ title }) => (
           <Col className="gutter-row" span={6}>
             <Card
               bordered={false}
